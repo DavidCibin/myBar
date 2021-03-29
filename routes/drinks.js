755cb95bfa0ce6fdf3d8406039e44bb8c973ebda
davidisurf <davidisurf@hotmail.com>
2021-03-29 15:47:30 -0400

    Logo, colors and slider changes

--- routes/drinks.js
+++ routes/drinks.js
@@ -15,14 +15,6 @@ router.get('/search', drinksCtrl.search)
 router.post("/:id", drinksCtrl.createReview)
 
 
-// router.get("/:slug", isLoggedIn, drinksCtrl.show)
-// router.post("/search", isLoggedIn, drinksCtrl.search)
-// router.post("/:slug/watch", isLoggedIn, drinksCtrl.addToWatchList)
-// router.post("/:slug/collection", isLoggedIn, drinksCtrl.addToCollection)
-// router.post("/:id/review", isLoggedIn, drinksCtrl.createReview)
-// router.delete("/:slug/watch", isLoggedIn, drinksCtrl.removeFromWatchList)
-// router.delete("/:slug/collection", isLoggedIn, drinksCtrl.removeFromCollection)
-
 function isLoggedIn(req, res, next) {
   if (req.isAuthenticated()) return next();
   res.redirect("/auth/google");

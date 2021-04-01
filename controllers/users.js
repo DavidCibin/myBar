f059e17eddfd003a424f1f7fe7656ce65f61325a
davidisurf <davidisurf@hotmail.com>
2021-03-31 21:10:33 -0400

    Add saved drinks to profile

--- controllers/users.js
+++ controllers/users.js
@@ -4,13 +4,41 @@ const Drink = require("../models/drink");
 module.exports = {
     index,
     showMyBooze,
+    // savedBooze,
 };
+
+
+
+// function savedBooze(req, res) {
+//     Drink.find({ saved: req.params.drinkId })
+//     console.log(req.params.drinkId)
+//         .then((saves) => {
+//             User.findById(req.user._id)
+//                 .populate('saved')
+//                 .then((user) => {
+//                     res.render("users/mybooze", {
+//                         saves,
+//                         title: 'Profile Page',
+//                         user
+//                     })
+//                 })
+//         })
+// }
+
+function index(req, res) {
+    User.find({}).then((users) => {
+        res.render('users/index', {
+            user: req.user, user,
+            title: 'Users Index'
+        })
+    })
+}
 
 function showMyBooze(req, res) {
     Drink.find({ postedBy: req.user._id })
         .then((drinks) => {
             User.findById(req.user._id)
-                .populate("postedBy")
+                .populate("drinks").exec()
                 .then((user) => {
                     res.render("users/mybooze", {
                         user,
@@ -22,11 +50,3 @@ function showMyBooze(req, res) {
         })
 }
 
-function index(req, res) {
-    User.find({}).then((users) => {
-        res.render('users/index', {
-            user: req.user, user,
-            title: 'Users Index'
-        })
-    })
-}

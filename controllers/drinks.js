70bc9dff19f7fc4c42a838a67cdc097624385b24
davidcibin <ddcibin@gmail.com>
2021-05-20 22:48:09 -0400

    code review

--- controllers/drinks.js
+++ controllers/drinks.js
@@ -88,7 +88,7 @@ function delDrink(req, res, next) {
 }
 
 function show(req, res) {
-    // console.log(req.params.id)
+    // console.log(req.params.id) //GET ALL INFO
     Drink.findById(req.params.id)
         .populate({ path: 'reviews.postedBy', model: 'User' })
         .populate({ path: 'postedBy', model: 'User' })
